9d2e6c23e76be58c990e288cd50571e1482224a1
jminguely <jminguely@gmail.com>
2025-11-25 17:11:36 +0100

    improve the project listing

--- wordpress/web/app/mu-plugins/wp-headless/inc/custom-post-type.php
+++ wordpress/web/app/mu-plugins/wp-headless/inc/custom-post-type.php
@@ -30,3 +30,31 @@ function wp_headless_register_project_type()
   register_post_type('project', $args);
 }
 add_action('init', 'wp_headless_register_project_type');
+
+add_filter('manage_project_posts_columns', function ($columns) {
+  $new_columns = [];
+  foreach ($columns as $key => $value) {
+    if ($key === 'title') {
+      $new_columns['thumbnail'] = __('Image');
+    }
+    $new_columns[$key] = $value;
+
+    if ($key === 'title') {
+      $new_columns['annee'] = __('Année');
+      $new_columns['lieu'] = __('Lieu');
+    }
+  }
+  return $new_columns;
+});
+
+add_action('manage_project_posts_custom_column', function ($column, $post_id) {
+  if ($column === 'thumbnail') {
+    echo get_the_post_thumbnail($post_id, [50, 50]);
+  }
+  if ($column === 'annee') {
+    echo get_field('annee', $post_id);
+  }
+  if ($column === 'lieu') {
+    echo get_field('lieu', $post_id);
+  }
+}, 10, 2);
